f830ec9a98a8d8094a82da076e9631a2ba04c503
Gobinda Chandra <g.chandra.2501@gmail.com>
2022-07-02 08:49:13 +0530

    x

--- anim.py
+++ anim.py
@@ -641,20 +641,4 @@ class DoubleArrowEx2(Scene):
         self.play(
             Create(grp)
         )
-        self.wait()
-
-
-from manim import*
-
-class DashedLineEx(Scene):
-    def construct(self):
-        d1=DashedLine(config.left_side,config.right_side,dash_length=2.0)
-        d1.set_color_by_gradient(RED,BLUE,GREEN,MAROON,GOLD)
-        d2=DashedLine(config.left_side,config.right_side)
-        d2.set_color_by_gradient(RED,BLUE,GREEN,MAROON,GOLD)
-        d3=DashedLine(config.left_side,config.right_side,dashed_ratio=0.2)
-        d3.set_color_by_gradient(RED,BLUE,GREEN,MAROON,GOLD)
-
-        self.play(
-            Create(d1)
-        )
+        self.wait()    
